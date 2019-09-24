a005173cac9bf2a2d6c7cd9ec5f6c1d572d7daf4
Sharon Watkins <swatkins876@gmail.com>
2019-09-24 14:14:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,4 +29,16 @@ class Song
     @@artists.uniq
   end
   
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) {
+      |total, i| total[i] += 1; total
+    }
+  end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) {
+      |total, i| total[i] += 1; total
+    }
+  end
+  
 end 
